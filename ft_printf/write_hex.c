88bceb732251790787c562c58c1d4ee42a4647a6
Brian Bauer <bbauer@e1z2r5p6.42.us.org>
2017-01-31 15:42:46 -0800

    merp

--- ft_printf/write_hex.c
+++ ft_printf/write_hex.c
@@ -6,7 +6,7 @@
 /*   By: bbauer <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/31 11:50:02 by bbauer            #+#    #+#             */
-/*   Updated: 2017/01/31 12:12:10 by bbauer           ###   ########.fr       */
+/*   Updated: 2017/01/31 15:41:51 by bbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 void				write_hex(t_conversion *conversion, va_list ap, t_format
 																	*format)
 {
+	char	*draft;
 	if (conversion && format)
-		ft_putstr(ft_itoa_base(va_arg(ap, int), 16));
+		draft = ft_itoa_base(va_arg(ap, int), 16);
+	ft_putstr(draft);
+	format->chars_written += ft_strlen(draft);
 }
