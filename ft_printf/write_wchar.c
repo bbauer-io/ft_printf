70a3ba4c86a623fd4f35e2797cf615643db881d8
Brian Bauer <bbauer@e1z3r9p9.42.us.org>
2017-03-04 10:12:02 -0800

    null wchars

--- ft_printf/write_wchar.c
+++ ft_printf/write_wchar.c
@@ -6,17 +6,39 @@
 /*   By: bbauer <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/16 22:39:43 by bbauer            #+#    #+#             */
-/*   Updated: 2017/02/17 02:42:31 by bbauer           ###   ########.fr       */
+/*   Updated: 2017/03/04 10:10:51 by bbauer           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static void			cleanup_memory(wchar_t **draft, char **utf8_draft,
+												char **utf8_draft_post_null)
+{
+		ft_memdel((void **)draft);
+		ft_memdel((void **)utf8_draft);
+		ft_memdel((void **)utf8_draft_post_null);
+}
+
+static void			print_null_wchar(t_conversion *conversion, t_format *format,
+									wchar_t *draft, char **utf8_draft_post_null)
+{
+	*utf8_draft_post_null = ft_utf8strencode(draft + ft_wstrlen(draft) + 1);
+	ft_putchar('\0');
+	format->chars_written++;
+	if (ft_wstrlen(draft) + 1 < conversion->width)
+	{
+		ft_putstr(*utf8_draft_post_null);
+		format->chars_written += ft_wstrlen(&draft[ft_wstrlen(draft) + 2]);
+	}
+}
 
 void				write_wchar(t_conversion *conversion, va_list ap,
 															t_format *format)
 {
 	wchar_t		*draft;
 	char		*utf8_draft;
+	char		*utf8_draft_post_null;
 
 	if (conversion->flags.hash)
 		return ;
@@ -25,11 +47,17 @@ void				write_wchar(t_conversion *conversion, va_list ap,
 		draft = (wchar_t *)malloc(sizeof(wchar_t) * 2);
 		*draft = va_arg(ap, wint_t);
 		draft[1] = '\0';
+		if (!conversion->width & conversion->flags.pos_values_begin_w_space)
+		{
+			ft_putchar(' ');
+			format->chars_written++;
+		}
 		apply_width_wchar(conversion, &draft);
 		utf8_draft = ft_utf8strencode(draft);
 		ft_putstr(utf8_draft);
 		format->chars_written += ft_wstrlen(draft);
-		ft_memdel((void **)&draft);
-		ft_memdel((void **)&utf8_draft);
+		if (*draft == '\0')
+			print_null_wchar(conversion, format, draft, &utf8_draft_post_null);
+		cleanup_memory(&draft, &utf8_draft, &utf8_draft_post_null);
 	}
 }
